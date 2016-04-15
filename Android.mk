c73e64ba1d68025d7df1183c4555119a3a47f94c
CTXz <ctx.xda@gmail.com>
2016-04-15 12:06:35 +0200

    kitakami: Add patcher
    I've written a patcher that can replace entire source files or directories. This allows for extremely flexible patching/modifying of source files.

---
+++ Android.mk
@@ -0,0 +1,52 @@
+LOCAL_PATH := device/patch/sony/kitakami
+
+VOLD_DIR := $(LOCAL_PATH)/patched/system/vold
+TORCH_DIR := $(LOCAL_PATH)/patched/frameworks/base/packages/SystemUI/src/com/android/systemui/statusbar/policy
+
+ifeq ($(SOMC_PLATFORM), kitakami)
+
+ifneq ($(wildcard $(LOCAL_PATH)/PATCHED.1),)
+
+$(shell touch $(LOCAL_PATH)/PATCHED.1)
+
+$(shell echo "Applying Vold patch")
+
+$(shell rm $(VOLD_DIR)/Utils.cpp)
+$(shell rm $(VOLD_DIR)/Android.mk)
+
+$(shell cp $(LOCAL_PATH)/$(VOLD_DIR)/Utils.cpp $(VOLD_DIR)/)
+$(shell cp $(LOCAL_PATH)/$(VOLD_DIR)/Android.mk $(VOLD_DIR))
+
+$(shell echo "Applying torch patch" )
+
+$(shell cp $(LOCAL_PATH)/$(TORCH_DIR)/FlashlightController.java $(TORCH_DIR)/)
+
+endif
+
+elseif ($(wildcard $(LOCAL_PATH)/PATCHED.1)
+
+$(shell mv $(LOCAL_PATH)/PATCHED.1 $(LOCAL_PATH)/PATCHED.0)
+endif
+
+  ifeq ($(wildcard $(LOCAL_PATH)/PATCHED.0)
+ 
+   $(shell echo "Not building kitakami! Reverting kitakami patches!")
+   
+   $(shell rm $(LOCAL_PATH)/PATCHED.0)
+   
+$(shell echo "Reverting vold patches")
+   
+$(shell rm $(VOLD_DIR)/Utils.cpp)
+$(shell rm $(VOLD_DIR)/Android.mk)
+
+$(shell echo "Reverting Flashlight patches")
+
+$(shell rm $(TORCH_DIR)/FlashlightController.java)
+
+$(shell echo "Fetching unpatched versions")
+
+$(shell repo sync --force-sync)
+
+$(shell echo "Patches reverted!")
+
+endif
