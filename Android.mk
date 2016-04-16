38280e397306afa47312641f630764dc233867a8
CTXz <ctx.xda@gmail.com>
2016-04-16 22:57:58 +0200

    Patcher: Fix makefile

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := device/patch/sony/kitakami
 
 VOLD_DIR := $(LOCAL_PATH)/patched/system/vold
-TORCH_DIR := $(LOCAL_PATH)/patched/frameworks/base/packages/SystemUI/src/com/android/systemui
+TORCH_DIR := $(LOCAL_PATH)/patched/frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles
 
 ifeq ($(SOMC_PLATFORM), kitakami)
 
@@ -14,14 +14,14 @@ ifeq ($(SOMC_PLATFORM), kitakami)
 		$(shell rm $(VOLD_DIR)/Utils.cpp)
 		$(shell rm $(VOLD_DIR)/Android.mk)
 
-		$(shell cp $(LOCAL_PATH)/$(VOLD_DIR)/Utils.cpp $(VOLD_DIR)/)
-		$(shell cp $(LOCAL_PATH)/$(VOLD_DIR)/Android.mk $(VOLD_DIR))
+		$(shell cp $(LOCAL_PATH)/patched/$(VOLD_DIR)/Utils.cpp $(VOLD_DIR)/)
+		$(shell cp $(LOCAL_PATH)/patched/$(VOLD_DIR)/Android.mk $(VOLD_DIR)/)
 
 		$(shell echo "Applying torch patch" )
 
-		$(shell rm $(TORCH_DIR)/qs/tiles/FlashlightTile.java)
+		$(shell rm $(TORCH_DIR)/FlashlightTile.java)
 
-		$(shell cp $(LOCAL_PATH)/$(TORCH_DIR)/qs/tiles/FlashlightTile.java $(TORCH_DIR)/qs/tiles/)
+		$(shell cp $(LOCAL_PATH)/patched/$(TORCH_DIR)/FlashlightTile.java $(TORCH_DIR)/)
 
 	endif
 
@@ -44,7 +44,7 @@ ifeq ($(wildcard $(LOCAL_PATH)/PATCHED.0)
 
 	$(shell echo "Reverting Flashlight patches")
 
-	$(shell rm $(TORCH_DIR)/qs/tiles/FlashlightTile.java)
+	$(shell rm $(TORCH_DIR)/FlashlightTile.java)
 
 	$(shell echo "Fetching unpatched versions")
 
