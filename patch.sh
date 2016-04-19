1ac7aca5213e1dc0eddc745dc1f906991e0098b4
CTXz <ctx.xda@gmail.com>
2016-04-19 21:45:35 +0200

    patcher: Avoid patcher conflicts
    If in some very rare cases devices need files patched that are already included by another patcher, the patcher will no longer delte those patched files and replace them with original ones. Instead it ignores the removal and lets the other patcher do its overwriting.
    
    * Added un-patched/untouched/original files backup function so a repo sync is no longer required
    
    * Added patchlists to avoid patch conflicts
    
    * Added device identifier to avoid unwanted patch removal
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- patch.sh
+++ patch.sh
@@ -3,30 +3,61 @@
 LOCAL_PATH=$(dirname $(readlink -f $0))
 TOP=$LOCAL_PATH/../../../..
 
-FRAMEWORKS_BASE_DIR=${TOP}/frameworks/base
 TORCH_DIR=${TOP}/frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles
-VOLD_DIR=${TOP}/system/vold
+RVRT_TORCH_DIR=${TORCH_DIR}/backup
 
+VOLD_DIR=${TOP}/system/vold
+RVRT_VOLD_DIR=${VOLD_DIR}/backup
 
 PATCHED_VOLD_DIR=${LOCAL_PATH}/patched/system/vold
 PATCHED_TORCH_DIR=${LOCAL_PATH}/patched/frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles
 
+function make_patch_stamp
+{
+	# Echo patch list ($1 = Files, $2 = path/to/patchlist)
+	echo "$1" > $2/patchlist
+
+	# Echo device mark 
+	echo "kitakami" > $2/patch-device
+}
+
 if [ ! -f {LOCAL_PATH}/PATCHED ]; then
 
 	touch ${LOCAL_PATH}/PATCHED
+	mkdir ${LOCAL_PATH}/backup
 
 	echo "Applying Vold patch!"
 
-	rm ${VOLD_DIR}/Utils.cpp
-	rm ${VOLD_DIR}/Android.mk
+	if [ ! -f ${VOLD_DIR}/patchlist]
+		make_patch_stamp 'Utils.cpp Android.mk' '${VOLD_DIR}'
+	fi
 
+	# Move original/untouched/un-patched files to backup dir
+	if [ ! -d ${RVRT_VOLD_DIR} ]; then
+
+		mkdir ${RVRT_VOLD_DIR}
+		echo "THIS DIR CONTAINS UNTOUCHED/UNPATCHED FILES, DO NOT REMOVE! THIS DIR WILL GET REMOVED AUTOMATICALLY IF NECCESSARY" > README
+		mv ${VOLD_DIR}/Utils.cpp ${RVRT_VOLD_DIR}/
+		mv ${VOLD_DIR}/Android.mk ${RVRT_VOLD_DIR}/
+	
+	fi
+
+	# Copy patched files to CM source
 	cp ${PATCHED_VOLD_DIR}/Utils.cpp ${VOLD_DIR}/
 	cp ${PATCHED_VOLD_DIR}/Android.mk ${VOLD_DIR}/
 
 	echo "Applying torch patch!"
 
-	rm ${TORCH_DIR}/FlashlightTile.java
+	# Move original/untouched/un-patched files to backup dir
+	if [ ! -d ${RVRT_TORCH_DIR} ]; then
 
+		mkdir ${RVRT_TORCH_DIR}
+		echo "THIS DIR CONTAINS UNTOUCHED/UNPATCHED FILES, DO NOT REMOVE! THIS DIR WILL GET REMOVED AUTOMATICALLY IF NECCESSARY" > README
+		mv ${TORCH_DIR}/FlashlightTile.java ${RVRT_TORCH_DIR}/
+	
+	fi
+
+	# Moved patched files to CM source
 	cp ${PATCHED_TORCH_DIR}/FlashlightTile.java ${TORCH_DIR}/
 
 fi
--- revert.sh
+++ revert.sh
@@ -3,10 +3,11 @@
 LOCAL_PATH=$(dirname $(readlink -f $0))
 TOP=$LOCAL_PATH/../../../..
 
-FRAMEWORKS_BASE_DIR=${TOP}/frameworks/base
 TORCH_DIR=${TOP}/frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles
-VOLD_DIR=${TOP}/system/vold
+RVRT_TORCH_DIR=${TORCH_DIR}/backup
 
+VOLD_DIR=${TOP}/system/vold
+RVRT_VOLD_DIR=${VOLD_DIR}/backup
 
 PATCHED_VOLD_DIR=${LOCAL_PATH}/patched/system/vold
 PATCHED_TORCH_DIR=${LOCAL_PATH}/patched/frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles
@@ -17,18 +18,43 @@ if [ -f ${LOCAL_PATH}/PATCHED ]; then
    
 	rm ${LOCAL_PATH}/PATCHED
    
-	echo "Reverting vold patches!"
-   
-	rm -R ${VOLD_DIR}
+   if [ -d ${VOLD_DIR}/patchlist ]; then
 
-	echo "Reverting Flashlight patches!"
+   		if grep -Fxq "Utils.cpp" ${VOLD_DIR}/patchlist then
 
-	rm -R ${FRAMEWORKS_BASE_DIR}
+   			if [ $(cat ${VOLD_DIR}/patch-device) = "kitakami"]; then
+   			
+   				# Revert original files
+				mv ${RVRT_VOLD_DIR}/Utils.cpp ${VOLD_DIR}/
+			
+			fi
+		fi
 
-	echo "Fetching unpatched versions!"
+		if grep -Fxq "Android.mk" ${VOLD_DIR}/patchlist then
 
-	eval "repo sync --force-sync"
+			if [ $(cat ${VOLD_DIR}/patch-device) = "kitakami"]; then
 
-	echo "Patches reverted!"
+				mv ${RVRT_VOLD_DIR}/Android.mk ${VOLD_DIR}/
 
+				rm -R ${RVRT_VOLD_DIR}
+
+			fi
+		fi
+	fi
+
+	if [ -d ${TORCH_DIR}/patchlist ]; then
+		if grep -Fxq "FlashlightTile.java" ${TORCH_DIR}/patchlist then
+   			if [ $(cat ${TORCH_DIR}/patch-device) = "kitakami"]; then
+
+   				# Revert original files
+				mv ${RVRT_TORCH_DIR}/FlashlightTile.java ${TORCH_DIR}/
+				rm -R ${RVRT_TORCH_DIR}
+
+			else
+
+				echo "Overlapping patches!!! Patches overwritten!"
+
+			fi
+		fi
+	fi
 fi
